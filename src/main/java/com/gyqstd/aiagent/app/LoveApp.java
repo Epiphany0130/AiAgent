a1ed7f20ba921f5c811e8057a848c26bfa05ad88
Epiphany0130 <sdguyq@163.com>
2025-10-15 18:43:01 +0800

    恋爱应用大师，基于内存实现多轮对话

---
+++ src/main/java/com/gyqstd/aiagent/app/LoveApp.java
@@ -0,0 +1,68 @@
+package com.gyqstd.aiagent.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
+
+/**
+ * @author GuYuqi
+ * @version 1.0
+ */
+@Component
+@Slf4j
+public class LoveApp {
+
+    // 注入 ChatClient，支持多轮对话
+    private final ChatClient chatClient;
+
+    private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
+                         "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
+                         "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
+                         "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
+
+    /**
+     * 构造器初始化 ChatClient
+     * @param dashscopeChatModel
+     */
+    private LoveApp(ChatModel dashscopeChatModel) {
+        // 初始化基于内存的对话记忆
+        ChatMemory chatMemory = new InMemoryChatMemory();
+        chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory)
+                )
+                .build();
+    }
+
+
+
+    /**
+     * 对话方法。调用 chatClient 对象，传入用户 prompt，并给 advisor 指定对话 id 和记忆大小
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChat(String message, String chatId) {
+        ChatResponse response = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+        String content = response.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
+}
---
+++ src/test/java/com/gyqstd/aiagent/app/LoveAppTest.java
@@ -0,0 +1,37 @@
+package com.gyqstd.aiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是谷煜奇";
+        String answer = loveApp.doChat(message, chatId);
+
+        // 第二轮
+        message = "你是谁";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+
+        // 第三轮
+        message = "你还记得我是谁吗";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+
+    }
+
+}
