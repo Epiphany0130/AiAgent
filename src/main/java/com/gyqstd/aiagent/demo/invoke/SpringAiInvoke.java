1cfba3d414c2281e9b79a8498da360e7e0655da4
Epiphany0130 <sdguyq@163.com>
2025-10-16 11:18:07 +0800

    自定义日志 Advisor

--- src/main/java/com/gyqstd/aiagent/demo/invoke/SpringAiInvoke.java
+++ src/main/java/com/gyqstd/aiagent/demo/invoke/SpringAiInvoke.java
@@ -1,6 +1,7 @@
 package com.gyqstd.aiagent.demo.invoke;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
