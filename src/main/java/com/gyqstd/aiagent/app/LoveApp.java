58051e8eee4ce0bc4a4558a5975ff721d6a5fa9b
Epiphany0130 <sdguyq@163.com>
2025-10-16 11:30:59 +0800

    自定义日志 Advisor

--- src/main/java/com/gyqstd/aiagent/app/LoveApp.java
+++ src/main/java/com/gyqstd/aiagent/app/LoveApp.java
@@ -1,5 +1,6 @@
 package com.gyqstd.aiagent.app;
 
+import com.gyqstd.aiagent.advisor.MyLoggerAdvisor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -39,7 +40,8 @@ public class LoveApp {
         chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory)
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new MyLoggerAdvisor()
                 )
                 .build();
     }
